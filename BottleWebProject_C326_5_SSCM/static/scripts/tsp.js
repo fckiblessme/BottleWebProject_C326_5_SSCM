ad955c5313433be51eabd41d9e2fbfd07845c447
DarSkry <daryaskry@gmail.com>
2026-06-02 23:47:55 +0300

    Добавление построения графа

--- BottleWebProject_C326_5_SSCM/static/scripts/tsp.js
+++ BottleWebProject_C326_5_SSCM/static/scripts/tsp.js
@@ -32,3 +32,93 @@ function unhighlightPair(i, j) {
     }
 }
 
+let networkInstance = null;
+
+function drawTSPGraph(matrix, bestRoute) {
+
+    const graphContainer = document.getElementById('network-graph');
+
+    if (!graphContainer) return;
+
+    const cityCount = matrix.length;
+
+    const nodesArray = [];
+    const edgesArray = [];
+
+    const selectedEdges = new Set();
+
+    for (let i = 0; i < bestRoute.length - 1; i++) {
+        const a = bestRoute[i];
+        const b = bestRoute[i + 1];
+        selectedEdges.add(a + "_" + b);
+        selectedEdges.add(b + "_" + a);
+    }
+
+    for (let i = 1; i <= cityCount; i++) {
+        nodesArray.push({
+            id: i,
+            label: String(i),
+            color: {
+                background: '#41444B',
+                border: '#41444B',
+                highlight: {
+                    background: '#41444B',
+                    border: '#DC3545'
+                },
+                hover: {
+                    background: '#41444B',
+                    border: '#41444B'
+                }
+            },
+            shape: 'circle',
+            font: {
+                size: 14,
+                color: '#DCD5C5',
+                face: 'monospace',
+                bold: true
+            }
+        });
+    }
+
+    for (let i = 0; i < cityCount; i++) {
+        for (let j = i + 1; j < cityCount; j++) {
+            const from = i + 1;
+            const to = j + 1;
+            const routeEdge = selectedEdges.has(from + "_" + to);
+
+            edgesArray.push({
+                from: from,
+                to: to,
+                label: String(matrix[i][j]),
+                width: routeEdge ? 4 : 2,
+                color: {
+                    color: routeEdge ? '#dc3545' : '#848484'
+                }
+            });
+        }
+    }
+
+    const data = {
+        nodes: new vis.DataSet(nodesArray),
+        edges: new vis.DataSet(edgesArray)
+    };
+
+    const options = {
+        physics: {
+            enabled: true,
+            stabilization: true
+        },
+        interaction: {
+            hover: true
+        }
+    };
+
+    if (networkInstance) {
+        networkInstance.destroy();
+    }
+
+    networkInstance = new vis.Network(graphContainer, data, options);
+}
+
+
+
